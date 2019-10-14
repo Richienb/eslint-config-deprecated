6ecec56294c8853a005e3a3a7e95d7c1558cdb9d
Richie Bendall <richiebendall@gmail.com>
2019-10-14 18:12:52 +1300

    feat: Add rules
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # eslint-config-deprecated
- 
+
+ESLint configuration for excluding deprecated rules.
+
+## Usage example
+
+First, run `npm install eslint-config-deprecated -D` or in yarn: `yarn add eslint-config-deprecated -D`
+
+Then setup your eslint config:
+
+```json
+{
+    "extends": ["eslint:recommended", "deprecated"]
+}
+```
---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = {
+    rules: {
+        "indent-legacy": 0,
+        "lines-around-directive": 0,
+        "newline-after-var": 0,
+        "newline-before-return": 0,
+        "no-catch-shadow": 0,
+        "no-native-reassign": 0,
+        "no-negated-in-lhs": 0,
+        "no-spaced-func": 0,
+        "prefer-reflect": 0,
+        "require-jsdoc": 0,
+        "valid-jsdoc": 0
+    }
+}
